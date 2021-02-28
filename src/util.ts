8681b6afa1f8c68ab23d354b9003e322e8f466d3
Sebastian Dittrich <sebastian.dittrich@live.de>
2021-02-28 17:47:35 +0100

    fixed dependencies

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-export function getInheritedPropertyDescriptor(object: object, property: string | number): PropertyDescriptor | undefined {
-    if (object == null) {
-        return undefined
-    }
-    return Object.getOwnPropertyDescriptor(object, property) || getInheritedPropertyDescriptor(Object.getPrototypeOf(object), property)
-}
-
-export type Constructor<T = {}> = new (...args: any[]) => T
